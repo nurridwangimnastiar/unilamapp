6642674aeaa121f9e3131701f445d4f53370e9a2
gimnastiar nur ridwan <nurridwangimnastiar@gmail.com>
2026-05-14 11:15:27 +0700

    http di ganti dengan dio

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,16 +1,17 @@
 import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart'; // GANTI http KE dio
 import '../models/berita_model.dart';
 
 class ApiService {
   static const String baseUrl = 'https://unilam.ac.id/api/v1';
+  static final Dio _dio = Dio(); // Inisialisasi Dio
 
   // Ambil daftar berita per halaman (1 - 19)
   static Future<List<BeritaModel>> getBeritaByPage(int page) async {
     try {
-      final response = await http.get(Uri.parse('$baseUrl/berita?page=$page'));
+      final response = await _dio.get('$baseUrl/berita?page=$page');
       if (response.statusCode == 200) {
-        final data = json.decode(response.body);
+        final data = response.data; // Dio otomatis decode JSON
         final List<dynamic> list = data['datas']['data'];
         return list.map((json) => BeritaModel.fromJson(json)).toList();
       }
@@ -23,9 +24,9 @@ class ApiService {
   // Ambil detail berita berdasarkan permalink
   static Future<BeritaModel?> getBeritaDetail(String permalink) async {
     try {
-      final response = await http.get(Uri.parse(permalink));
+      final response = await _dio.get(permalink);
       if (response.statusCode == 200) {
-        final data = json.decode(response.body);
+        final data = response.data; // Dio otomatis decode JSON
         return BeritaModel.fromJson(data['data']);
       }
       return null;
